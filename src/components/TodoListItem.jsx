fc5c4f52651b06e3f19a7df7f15236816205c0e7
CarstenHennig <carsten.hennig@gmail.com>
2021-08-12 11:08:51 +0200

    Almost done

--- src/components/TodoListItem.jsx
+++ src/components/TodoListItem.jsx
@@ -1,15 +1,15 @@
 import { useContext } from 'react';
 import { UserContext } from '../contexts/UserContext'
-import { useState } from 'react';
 
 function TodoListItem({ todo, deleteTodo }) {
 
-    const { user, setUser } = useContext(UserContext);
-    const { changeColor, setChangeColor } = useState(false);
+    const { changeColor } = useContext(UserContext);
 
-    const mine = todo.user === user.id;
+    // const mine = todo.user === user.id;
+    const mine = changeColor === "light" ? "my-todo" : "list-background";
+
     return(
-        <li className={mine ? "my-todo" : ""} value="dark">
+        <li className={changeColor ? "my-todo" : "list-background"} value="dark">
             {todo.done ? "✓ " : "○ "}
             {todo.text}
             {mine &&
